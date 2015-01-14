5dfd17d6b10739107d8857b1e0bd26121b464b4c
Edward Waked <ebwaked87@gmail.com>
2015-01-14 10:17:24 -0500

    HTML to MVC
    changed my resume from HTML to MVC, downloaded a new template for my
    resume to start working on.

---
+++ MVC Resume MIO/MVC Resume MIO/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Resume_MIO.Startup))]
+namespace MVC_Resume_MIO
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
