a808404f1d2d2c602c461a0da060a5d72e0614c9
Edward Waked <ebwaked87@gmail.com>
2015-01-16 12:50:51 -0500

    Updated Resume
    lots of change!

--- MVC Resume MIO/MVC Resume MIO/Controllers/HomeController.cs
+++ MVC Resume MIO/MVC Resume MIO/Controllers/HomeController.cs
@@ -13,7 +13,42 @@ namespace MVC_Resume_MIO.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Keypoints()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Experience()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Portfolio()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Skills()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Blog()
         {
             ViewBag.Message = "Your application description page.";
 
--- MVC Resume MIO/MVC Resume MIO/Models/IdentityModels.cs
+++ MVC Resume MIO/MVC Resume MIO/Models/IdentityModels.cs
@@ -3,13 +3,21 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
 
 namespace MVC_Resume_MIO.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DisplayName { get; set; }
+
+        public virtual ICollection<Comment> BlogComments { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
@@ -29,5 +37,16 @@ namespace MVC_Resume_MIO.Models
         {
             return new ApplicationDbContext();
         }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Post>().HasMany(c => c.Comments);
+            modelBuilder.Entity<ApplicationUser>().HasMany(u => u.BlogComments);
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
     }
 }
