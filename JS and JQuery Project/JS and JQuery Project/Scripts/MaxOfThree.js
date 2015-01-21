3e28d7b29d94442193dedb5d76e7c824d01531b9
Edward Waked <ebwaked87@gmail.com>
2015-01-21 16:42:53 -0500

    First modal done
    Finished first modal with syntax highlighter

---
+++ JS and JQuery Project/JS and JQuery Project/Scripts/MaxOfThree.js
@@ -0,0 +1,18 @@
+﻿function maxOfThree() {
+    var ans = 0;
+    var a = $('#maxNum1').val();
+    var b = $('#maxNum2').val();
+    var c = $('#maxNum3').val();
+
+        if (a >= b && a >= c){
+            ans = a;
+        } else if (b >= a && b >= c) {
+            ans = b;
+        } else {
+            ans = c;
+        }
+        ans = Number(ans);
+      $('#answer').replaceWith('<div id=\'answer\'><p>' + ans + '</p></div>');
+    }
+
+   
---
+++ JS and JQuery Project/JS and JQuery Project/Scripts/SumFunction.js
@@ -0,0 +1,45 @@
+﻿$('#sumNumbers');
+var sumNumbers = [];
+var sumList = sumNumbers.split(",");
+totalSum = 0;
+
+function sumNumbers(sumList) {
+    for (var i = 0; i < sumNumbers.length; i++) {
+
+        totalSum += sumList[i];
+        $('#sumAnswer').replaceWith('<div id=\'sumAnswer\'><p>' + totalSum + '</p></div>');
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//Sum Function
+//var numbers = [6, 7, 3, 4];
+//var totalAmount = 0;
+
+//for (var i = 0; i < numbers.length; i++) {
+
+//    totalAmount += numbers[i];
+//}
+//document.write(totalAmount);
+
+
+////Multiplier Function
+//    var numbers = [1, 2, 3, 4];
+//var totalAmount = 1;
+
+//for (var i = 0; i < numbers.length; i++) {
+
+//    totalAmount *= numbers[i];
+//}
+//document.write(totalAmount);
+//document.write('<br>');
