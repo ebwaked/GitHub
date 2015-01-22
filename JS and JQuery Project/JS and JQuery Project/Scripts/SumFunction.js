5d54e949084688d3c0528f0327d617c0af7414ab
Edward Waked <ebwaked87@gmail.com>
2015-01-22 10:27:01 -0500

    Multiply Modal
    Finished the javascript for the multiply function and finished the modal
    for it.

--- JS and JQuery Project/JS and JQuery Project/Scripts/SumFunction.js
+++ JS and JQuery Project/JS and JQuery Project/Scripts/SumFunction.js
@@ -1,7 +1,5 @@
-﻿
-
-function sumNumbers(sumList) {
-    var sumList = $('#sumNumbers').val().split(',');
+﻿function sumNumbers(sumList) {
+    var sumList = $('#sumNumbers').val().split('+');
     var sumTotal = 0;
         for (i = 0; i < sumList.length; i++) {
             sumTotal += parseInt(sumList[i]);     
@@ -18,24 +16,4 @@ function sumNumbers(sumList) {
 
 
 
-//Sum Function
-//var numbers = [6, 7, 3, 4];
-//var totalAmount = 0;
 
-//for (var i = 0; i < numbers.length; i++) {
-
-//    totalAmount += numbers[i];
-//}
-//document.write(totalAmount);
-
-
-////Multiplier Function
-//    var numbers = [1, 2, 3, 4];
-//var totalAmount = 1;
-
-//for (var i = 0; i < numbers.length; i++) {
-
-//    totalAmount *= numbers[i];
-//}
-//document.write(totalAmount);
-//document.write('<br>');
