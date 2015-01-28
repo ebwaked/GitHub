99f067d3da52b8869762e36e09f94259bdd0b2bf
Edward Waked <ebwaked87@gmail.com>
2015-01-28 11:38:36 -0500

    Revert "test commit"
    This reverts commit a11f47424483b449968ebfeabdf97be8f0236c92.
    
    Conflicts:
    	MVC Resume MIO/MVC Resume MIO/MVC Resume MIO.csproj
    	MVC Resume MIO/MVC Resume MIO/Views/Home/Index.cshtml
    	MVC Resume MIO/MVC Resume MIO/Views/Home/JavascriptExercises.cshtml

--- MVC Resume MIO/MVC Resume MIO/Scripts/JSExercises/JavaScript1.js
+++
@@ -1,4 +0,0 @@
-﻿function overlay() {
-    el = document.getElementById("overlay");
-    el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
-}
--- MVC Resume MIO/MVC Resume MIO/Scripts/JSExercises/MultiplyFunction.js
+++
@@ -1,9 +0,0 @@
-﻿function multiplyNumbers(multiplyList) {
-    var multiplyList = $('#multiplyNumbers').val().split("*");
-    var multiplyTotal = 1;
-    for (i = 0; i < multiplyList.length; i++) {
-        multiplyTotal *= parseInt(multiplyList[i]);
-    }
-    $('#multiplyAnswer').replaceWith('<div id=\'multiplyAnswer\'><p>' + multiplyTotal + '</p></div>');
-}
-
